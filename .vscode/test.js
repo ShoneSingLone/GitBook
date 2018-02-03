51bcf036bb3f0329fb902bc5fb239bc3d83886e5
ShoneSingLone <ShoneSingLone@gmail.com>
2018-02-03 22:53:17 +0800

    【update】

--- .vscode/test.js
+++ .vscode/test.js
@@ -1,8 +1,10 @@
 function foo(a, b) {
-    console.log("this.a", this.a);
-    console.log(a, b);
+    console.log("a:" + this.a + ", b:" + this.b);
 }
-foo();
-var baz = foo.bind({a:"foo.bind"}, 2);
-foo();
-baz(3);
+// 把数组“展开”成参数
+var a = "a",
+    b = "b";
+foo.apply(null, [2, 3]); // a:2, b:3
+// 使用 bind(..) 进行柯里化
+var bar = foo.bind(null, 2);
+bar(3); // a:2, b:3
