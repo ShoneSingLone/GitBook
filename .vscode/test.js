623b1c5631f78ecab7093337ba275a3448452c90
ShoneSingLone <ShoneSingLone@gmail.com>
2018-02-14 19:21:23 +0800

    【update】

--- .vscode/test.js
+++ .vscode/test.js
@@ -1,6 +1,4 @@
-var a = "foo!";
-var b = Array.prototype.join.call(a, "hehe");
-console.log(a);
-var c = (b % 'a');
-console.log(c === c);
-console.log(Object.prototype.toString.call(c));
+console.log([] == '')
+// console.log('1' == 1)
+// console.log({} == '[object Object]')
+// console.log(NaN != NaN)
